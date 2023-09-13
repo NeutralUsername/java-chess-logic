4655626b5be7fe0e5377f87f8091da560f1b49ee
NeutralUsername <stuck.fabian@gmail.com>
2023-09-13 10:03:14 +0200

    refactoring

--- Chess.java
+++ Chess.java
@@ -19,37 +19,7 @@ public class Chess {
     private long lastMoveDate = System.currentTimeMillis();
 
     public Chess() {
-        board = new Field[8][8];
-        for (int i = 0; i < 8; i++) {
-            board[1][i] = new Field(1, i, new Pawn(true));
-            board[6][i] = new Field(6, i, new Pawn(false));
-        }
-        board[0][0] = new Field(0, 0, new Rook(true));
-        board[0][1] = new Field(0, 1, new Knight(true));
-        board[0][2] = new Field(0, 2, new Bishop(true));
-        board[0][3] = new Field(0, 3, new Queen(true));
-        board[0][4] = new Field(0, 4, new King(true));
-        board[0][5] = new Field(0, 5, new Bishop(true));
-        board[0][6] = new Field(0, 6, new Knight(true));
-
-        board[0][7] = new Field(0, 7, new Rook(true));
-        board[7][0] = new Field(7, 0, new Rook(false));
-        board[7][1] = new Field(7, 1, new Knight(false));
-        board[7][2] = new Field(7, 2, new Bishop(false));
-        board[7][3] = new Field(7, 3, new Queen(false));
-        board[7][4] = new Field(7, 4, new King(false));
-        board[7][5] = new Field(7, 5, new Bishop(false));
-        board[7][6] = new Field(7, 6, new Knight(false));
-        board[7][7] = new Field(7, 7, new Rook(false));
-
-        for (int i = 2; i < 6; i++) {
-            for (int j = 0; j < 8; j++) {
-                board[i][j] = new Field(i, j, null);
-            }
-        }
-
-        timeWhite = 60*10 * 1000;
-        timeBlack = 60*10 * 1000;
+        this(15*60);
     }
 
     public Chess(int timeInSeconds) {
